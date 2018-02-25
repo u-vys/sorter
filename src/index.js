4ce443af608007a773434c77a6d311bec205e1b9
u-vys <vn.vysotski@gmail.com>
2018-02-25 19:32:08 +0300

    sorter-draft

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.array = [];
+        this.compareFunction;
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this.array.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.array[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.array.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this.array;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        var i;
+        var temp = [];
+        for (i = 0; i < indices.length; i++) {
+            temp.push(this.array[indices[i]]);
+        }
+        temp.sort(this.compareFunction);
+        for (i = 0; i < indices.length; i++) {
+            this.array[indices[i]] = temp[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this.compareFunction = compareFunction;
+    }
 }
 
 module.exports = Sorter;
